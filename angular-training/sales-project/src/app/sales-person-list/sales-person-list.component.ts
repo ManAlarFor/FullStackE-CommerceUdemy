3455b599c7208834551b1e402b644969579be0fd
Manuel <alarmanuel16@gmail.com>
2024-06-03 09:26:14 +0200

    Sección 8: "Angular Crash Course - Bootstrap CSS, Conditionals abd Formating"

--- angular-training/sales-project/src/app/sales-person-list/sales-person-list.component.ts
+++ angular-training/sales-project/src/app/sales-person-list/sales-person-list.component.ts
@@ -5,7 +5,7 @@ import { SalesPerson } from './sales-person';
   selector: 'app-sales-person-list',
   standalone: true,
   imports: [],
-  templateUrl: './sales-person-list.component.html',
+  templateUrl: './sales-person-list-bootstrap.component.html',
   styleUrl: './sales-person-list.component.css'
 })
 export class SalesPersonListComponent implements OnInit{
@@ -13,9 +13,9 @@ export class SalesPersonListComponent implements OnInit{
   //creating an array
   salesPersonList: SalesPerson[] = [
     new SalesPerson("Peter","Parker","spider@gmail.com",4300) ,
-    new SalesPerson("Ben","Riley","spaider@gmail.com",4300) ,
-    new SalesPerson("Miguel","O'Hara","spide2099r@gmail.com",4300) ,
-    new SalesPerson("Gwen","Stacy","drums@gmail.com",4300) ,
+    new SalesPerson("Ben","Riley","spaider@gmail.com",800) ,
+    new SalesPerson("Miguel","O'Hara","spide2099r@gmail.com",9000) ,
+    new SalesPerson("Gwen","Stacy","drums@gmail.com",6300) ,
   ]
 
   constructor() {}
